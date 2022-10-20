554a42e54afe63065576e5849db55af3923eadda
CalvinBauman <calvin.bauman@icloud.com>
2022-10-20 12:42:42 -0700

    update configuration

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,78 @@
 
 //Randomise the background of the quiz for each questiion
 
+const questions = [
+    {
+      id: "1",
+      question: "Please provide your age range.",
+      answer1: "under 18",
+      answer1Total: "1",
+      answer2: "18 - 30",
+      answer2Total: "2",
+      answer3: "over 30",
+      answer3Total: "3"
+    },
+    {
+      id: "2",
+      question: "How much free time do you have tonight?",
+      answer: "less than 2 hours",
+      answer1Total: "1",
+      answer2: "2-5 hours",
+      answer2Total: "2",
+      answer3: "I am unemployed with nothing to do",
+      answer3Total: "3"
+    },
+    {
+      id: "3",
+      question: "Do you want to watch a movie, play a game, or read a book?",
+      answer1: "Watch a movie",
+      answer1Total: "1",
+      answer2: "Play a game",
+      answer2Total: "3",
+      answer3: "Read a book",
+      answer3Total: "2"
+    },
+    {
+      id: "4",
+      question: "What genre seems most interesting to you?",
+      answer1: "Action and Adventure",
+      answer1Total: "3",
+      answer2: "Romance",
+      answer2Total: "2",
+      answer3: "Horror",
+      answer3Total: "1"
+    },
+    {
+      id: "5",
+      question: "Are you alone for this activity?",
+      answer1: "Yes and I plan to stay alone",
+      answer1Total: "1",
+      answer2: "No I have company tonight!",
+      answer2Total: "2",
+      answer3: "Undecided",
+      answer3Total: "3"
+    },
+    {
+      id: "6",
+      question: "Do you want to start an activity that will take up your time on future nights?",
+      answer1: "No I want to clear my mind and do something just for tonight",
+      answer1Total: "3",
+      answer2: "I want to start something that I can continue in the near future",
+      answer2Total: "2",
+      answer3: "I want to do something that will take up the majority of my time",
+      answer3Total: "1"
+    },
+    {
+      id: "7",
+      question: "Do you prioritize storytelling, visuals, or music?",
+      answer1: "Storytelling",
+      answer1Total: "1",
+      answer2: "Visuals",
+      answer2Total: "2",
+      answer3: "Music",
+      answer3Total: "3"
+    }
+  ]
 
 let currentQuestion = 0;
 let score = [];
