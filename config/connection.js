40f2353df487acfaf1ceab9c438b708a8c1aa3dd
Matthew Cole <matthewcole@Matthews-MacBook-Pro.local>
2022-10-20 17:32:49 -0700

    heroku work

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
-const { Client } = require('pg')
-const client = new Client({
-  user: 'jcnyqkajkckzoa',
-  host: 'ec2-44-209-24-62.compute-1.amazonaws.com',
-  database: 'dn9ri9mu85u05',
-  password: 'e138d07d08d90a42135c4846524a192bb748acdfc8010ed2ffcd117147fb26ef',
-  port: 5432,
-})
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+   
+  );
+}
 
 module.exports = sequelize;
