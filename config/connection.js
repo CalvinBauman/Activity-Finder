e17c2451d9f9f87137e45da46f48a4f06940a743
Matthew Cole <matthewcole@Matthews-MacBook-Pro.local>
2022-10-20 17:20:50 -0700

    heroku touchups

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,14 @@
-const dbConfig = require("./db.config");
-
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+const { Client } = require('pg')
+const client = new Client({
+  user: 'jcnyqkajkckzoa',
+  host: 'ec2-44-209-24-62.compute-1.amazonaws.com',
+  database: 'dn9ri9mu85u05',
+  password: 'e138d07d08d90a42135c4846524a192bb748acdfc8010ed2ffcd117147fb26ef',
+  port: 5432,
+})
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
 
 module.exports = sequelize;
